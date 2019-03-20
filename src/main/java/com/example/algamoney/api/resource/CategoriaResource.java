4602d02d3a25ab8e59ed81f2c9ed66de90093f7e
Victor Azevedo <victor_mouraazevedo@hotmail.com>
2019-03-20 18:17:31 -0300

    implementando POST

--- src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -4,10 +4,13 @@ package com.example.algamoney.api.resource;
 import com.example.algamoney.api.model.Categoria;
 import com.example.algamoney.api.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 import java.util.List;
 
 @RestController //com essa annotation, o retorno já vai ser convertido pra json
@@ -20,5 +23,19 @@ public class CategoriaResource {
     @GetMapping
     public List<Categoria> listar() {
         return categoriaRepository.findAll();
+    }
+
+    //if ok, 201 Created
+    @PostMapping
+    public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+        Categoria categoriaSalva = categoriaRepository.save(categoria);
+        //pega a partir da requisição atual, adiciona o codigo e adiciona o codigo na URI
+        //por padrao, o REST pede para termos uma location no header após dar um POST informando
+        //como retornar o objeto que acabamos de salvar
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+                .buildAndExpand(categoriaSalva.getCodigo()).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+
+        return ResponseEntity.created(uri).body(categoriaSalva);
     }
 }
