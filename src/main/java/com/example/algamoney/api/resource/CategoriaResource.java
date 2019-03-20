4a2dd3d123440a9529796cbd579d9bb6d416b675
Victor Azevedo <victor_mouraazevedo@hotmail.com>
2019-03-20 16:55:19 -0300

    realizando o primeiro Get da aplicação

---
+++ src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -0,0 +1,24 @@
+package com.example.algamoney.api.resource;
+
+
+import com.example.algamoney.api.model.Categoria;
+import com.example.algamoney.api.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController //com essa annotation, o retorno já vai ser convertido pra json
+@RequestMapping("/categorias")
+public class CategoriaResource {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @GetMapping
+    public List<Categoria> listar() {
+        return categoriaRepository.findAll();
+    }
+}
