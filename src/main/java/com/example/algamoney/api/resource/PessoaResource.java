75c75fa8cb0159be5f87ef51ab581a87472c5b39
Victor Azevedo <victor_mouraazevedo@hotmail.com>
2019-03-24 21:51:20 -0300

    implementação do location no body da Pessoa e do Get Pessoas

--- src/main/java/com/example/algamoney/api/resource/PessoaResource.java
+++ src/main/java/com/example/algamoney/api/resource/PessoaResource.java
@@ -5,8 +5,11 @@ import com.example.algamoney.api.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 @RequestMapping("/pessoas")
@@ -16,7 +19,20 @@ public class PessoaResource {
     private PessoaRepository pessoaRepository;
 
     @PostMapping
-    private ResponseEntity<Pessoa> cadastrarPessoa(@Valid @RequestBody Pessoa pessoa) {
-        return ResponseEntity.ok(pessoaRepository.save(pessoa));
+    private ResponseEntity<Pessoa> cadastrarPessoa(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
+        Pessoa pessoaSalva = pessoaRepository.save(pessoa);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+                .buildAndExpand(pessoaSalva.getCodigo()).toUri();
+        response.setHeader("Location", uri.toString());
+
+        return ResponseEntity.created(uri).body(pessoaSalva);
+    }
+
+    @GetMapping("/{codigo}")
+    private ResponseEntity<Pessoa> buscarPeloCodigo(@PathVariable Long codigo) {
+        Pessoa resultado = pessoaRepository.findOne(codigo);
+
+        return resultado != null ? ResponseEntity.ok(resultado) : ResponseEntity.notFound().build();
     }
 }
