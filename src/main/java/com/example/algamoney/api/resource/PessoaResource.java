96d477501a0849c3164ea3d8c83d58a9b88772a8
Victor Azevedo <victor_mouraazevedo@hotmail.com>
2019-03-24 21:08:39 -0300

    desafio cadastro de pessoas implementado (basico)

--- src/main/java/com/example/algamoney/api/resource/PessoaResource.java
+++ src/main/java/com/example/algamoney/api/resource/PessoaResource.java
@@ -1,9 +1,12 @@
 package com.example.algamoney.api.resource;
 
+import com.example.algamoney.api.model.Pessoa;
 import com.example.algamoney.api.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/pessoas")
@@ -11,4 +14,9 @@ public class PessoaResource {
 
     @Autowired
     private PessoaRepository pessoaRepository;
+
+    @PostMapping
+    private ResponseEntity<Pessoa> cadastrarPessoa(@Valid @RequestBody Pessoa pessoa) {
+        return ResponseEntity.ok(pessoaRepository.save(pessoa));
+    }
 }
